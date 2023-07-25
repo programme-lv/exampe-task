dbb96fd739181d3ac9e3029881cd637e62e9c8b8
KrisjanisP <krisjanispetrucena@gmail.com>
2023-07-25 21:33:02 +0300

    generated answers

--- summa/generation/generate.jl
+++ summa/generation/generate.jl
@@ -1,9 +1,11 @@
 GEN = ENV["GEN_EXE"]
+SOL = ENV["SOL_EXE"]
 
 for i = 1:10
     # filename 0001.in, 0002.in and so on
     testname = string(i,base=10,pad=3)
-    filename = "$(testname).in"
-    open(f->write(f, read(`$GEN $i`, String)), filename, "w")
+    
+    open(f->write(f, read(`$GEN $i`, String)), "$(testname).in", "w")
+    open(f->write(f, read(pipeline(`cat $(testname).in`,`$SOL`), String)), "$(testname).ans", "w")
 end
 
--- summa/scripts/generate.sh
+++ summa/scripts/generate.sh
@@ -3,6 +3,7 @@
 ROOT_DIR=$(readlink -f "$(dirname "$0")/..")
 TEMP_DIR="$ROOT_DIR/temporary"
 GEN_DIR="$ROOT_DIR/generation"
+SOL_DIR="$ROOT_DIR/solutions"
 
 if [ ! -f "$GEN_DIR/generator.cpp" ]; then
     echo "generator.cpp not found"
@@ -14,6 +15,11 @@ if [ ! -f "$GEN_DIR/generate.jl" ]; then
     exit 1
 fi
 
+if [ ! -f "$SOL_DIR/correct.cpp" ]; then
+    echo "correct.cpp not found"
+    exit 1
+fi
+
 if [ ! -d "$TEMP_DIR" ]; then
     echo "creating temporary directory"
     mkdir -p $TEMP_DIR
@@ -22,9 +28,16 @@ fi
 echo "compiling generator"
 g++ -o $TEMP_DIR/generator $GEN_DIR/generator.cpp
 
+echo "compiling solution"
+g++ -o $TEMP_DIR/solution $SOL_DIR/correct.cpp
+
 GEN_SCRIPT=$(readlink -f "$GEN_DIR/generate.jl")
 GEN_EXE=$(readlink -f "$TEMP_DIR/generator")
+SOL_EXE=$(readlink -f "$TEMP_DIR/solution")
 TEST_DIR="$ROOT_DIR/tests"
+
+export GEN_EXE
+export SOL_EXE
 
 if [ -d "$TEST_DIR" ]; then
     echo "removing old test directory"
@@ -34,6 +47,8 @@ fi
 mkdir -p $TEST_DIR
 
 echo "generating tests"
-pushd $TEST_DIR
+cd $TEST_DIR
 julia $GEN_SCRIPT
-popd
+
+echo "finished"
+
