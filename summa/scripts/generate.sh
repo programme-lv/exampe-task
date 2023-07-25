78a4af55eed310f19b5dc638140d933da8600e1a
KrisjanisP <krisjanispetrucena@gmail.com>
2023-07-25 20:54:14 +0300

    generate script

--- summa/scripts/generate.sh
+++ summa/scripts/generate.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
 ROOT_DIR=$(readlink -f "$(dirname "$0")/..")
-TEMP_DIR="$ROOT_DIR/tmp"
+TEMP_DIR="$ROOT_DIR/temporary"
 GEN_DIR="$ROOT_DIR/generation"
 
-g++ $GEN_DIR/generator.cpp -o $TEMP_DIR/generator
+if [ ! -f "$GEN_DIR/generator.cpp" ]; then
+    echo "generator.cpp not found"
+    exit 1
+fi
 
+if [ ! -f "$GEN_DIR/generate.jl" ]; then
+    echo "generate.jl not found"
+    exit 1
+fi
+
+if [ ! -d "$TEMP_DIR" ]; then
+    echo "creating temporary directory"
+    mkdir -p $TEMP_DIR
+fi
+
+echo "compiling generator"
+g++ -o $TEMP_DIR/generator $GEN_DIR/generator.cpp
+
+GEN_SCRIPT=$(readlink -f "$GEN_DIR/generate.jl")
 GEN_EXE=$(readlink -f "$TEMP_DIR/generator")
+TEST_DIR="$ROOT_DIR/tests"
 
-echo $GEN_EXE
+if [ -d "$TEST_DIR" ]; then
+    echo "removing old test directory"
+    rm -r $TEST_DIR
+fi
 
+mkdir -p $TEST_DIR
+
+echo "generating tests"
+pushd $TEST_DIR
+julia $GEN_SCRIPT
+popd
